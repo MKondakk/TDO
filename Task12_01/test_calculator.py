14fc9fb49f5a6b1e773ffbc80d7ac2afa692a81a
Mariia <mariia.kondak@nure.ua>
2024-06-21 00:03:26 +0200

    Some changes

--- Task12_01/test_calculator.py
+++ Task12_01/test_calculator.py
@@ -4,10 +4,11 @@ from calculator import multiply
 class TestCalculator(unittest.TestCase):
 
     def test_multiply(self):
-        self.assertEqual(multiply(2, 3), 6)
+        self.assertEqual(multiply(2, 4), 8)
         self.assertEqual(multiply(-1, 1), -1)
         self.assertEqual(multiply(-1, -1), 1)
         self.assertEqual(multiply(0, 1), 0)
 
 if __name__ == "__main__":
     unittest.main()
+
